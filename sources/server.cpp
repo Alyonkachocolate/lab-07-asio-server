e606e3919bd445c8b57be729fed0c8f396809b65
Alyonkachocolate <Nyanyoka@yandex.ru>
2021-01-19 01:01:04 +0400

    Update CMakeLists.txt

--- sources/server.cpp
+++ sources/server.cpp
@@ -87,9 +87,9 @@ void handle_clients_thread() {
   }
 }
 
-//int main() {
-//  boost::thread_group threads;
-//  threads.create_thread(accept_thread);
-//  threads.create_thread(handle_clients_thread);
-//  threads.join_all();
-//}
+int main() {
+  boost::thread_group threads;
+  threads.create_thread(accept_thread);
+  threads.create_thread(handle_clients_thread);
+  threads.join_all();
+}
